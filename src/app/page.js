421bc6d966bdf011142d3b19455ef375482debd1
navod98 <174487002+Imaudani@users.noreply.github.com>
2025-09-29 07:13:00 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 // import Dashboard from "./dashboard/page"
-import Register from "./register/page";
+// import Register from "./register/page";
+import Login from "./login/page"
 
 export default function Home() {
   return (
     <div>
-      <Register/>
+      <Login/>
     </div>
   );
 }
